9bc35d6be4db20958a611b0f72fecbf6778118a0
Fusion86 <fusion86@cerbus.nl>
2021-11-25 16:53:51 +0100

    Update

--- assignment_1/src/Calendar.hs
+++ assignment_1/src/Calendar.hs
@@ -1,23 +1,51 @@
 module Calendar where
 
-import ParseLib.Abstract
-import Prelude hiding ((<$), ($>), (<*), (*>), sequence)
 import DateTime
-
+import ParseLib.Abstract
+import Prelude hiding (sequence, ($>), (*>), (<$), (<*))
 
 -- Exercise 6
 data Calendar = Calendar
-    deriving (Eq, Ord, Show)
+  { properties :: [CalendarProp],
+    event :: [Event]
+  }
+  deriving (Eq, Ord)
 
-data Event = Event
-    deriving (Eq, Ord, Show)
+data Event
+  = DateTimeStamp DateTime
+  | Uid String
+  | DateTimeStart DateTime
+  | DateTimeEnd DateTime
+  | Description String
+  | Summary String
+  | Location String
+  deriving (Eq, Ord)
+
+data CalendarProp
+  = ProdId String
+  | Version
+  deriving (Eq, Ord)
 
 -- Exercise 7
 data Token = Token
-    deriving (Eq, Ord, Show)
+  { key :: String,
+    value :: String
+  }
+  deriving (Eq, Ord, Show)
 
 scanCalendar :: Parser Char [Token]
-scanCalendar = undefined
+scanCalendar = greedy parseToken
+  where
+    notNewline = satisfy (\c -> c /= '\r' && c /= '\n') 
+
+    parseToken :: Parser Char Token
+    parseToken = do
+      key <- greedy $ satisfy (/= ':')
+      symbol ':'
+      value <- greedy notNewline
+      optional $ symbol '\r'
+      symbol '\n'
+      return $ Token key value
 
 parseCalendar :: Parser Token Calendar
 parseCalendar = undefined
