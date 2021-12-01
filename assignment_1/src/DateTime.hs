53889b1e3043c0a3dbc66813f8fa678ff9620189
Fusion86 <fusion86@cerbus.nl>
2021-12-01 16:51:56 +0100

    Almost done

--- assignment_1/src/DateTime.hs
+++ assignment_1/src/DateTime.hs
@@ -92,11 +92,13 @@ checkDateTime (DateTime (Date y m d) (Time hh mm ss) _) =
     validRange min max actual = min <= actual && actual <= max
     validDay y m d = validRange 0 (daysInMonth y m) d
 
+daysInMonth :: Int -> Int -> Int
 daysInMonth y m
   | m == 2 = if isLeapYear y then 29 else 28
   | m `elem` [1, 3, 5, 7, 8, 10, 12] = 31
   | otherwise = 30
 
+isLeapYear :: Int -> Bool
 isLeapYear y = divBy 400 || (divBy 4 && not (divBy 100))
   where
     divBy x = mod y x == 0
@@ -104,6 +106,7 @@ isLeapYear y = divBy 400 || (divBy 4 && not (divBy 100))
 parseCheck :: [Char] -> Maybe Bool
 parseCheck s = checkDateTime <$> run parseDateTime s
 
+-- Might be incorrect, but we only need a relative difference so it doesn't really matter.
 dateTimeToTimestamp :: DateTime -> Int
 dateTimeToTimestamp
   (DateTime (Date (Year y) (Month m) (Day d)) (Time (Hour hh) (Minute mm) (Second ss)) _)
@@ -116,3 +119,51 @@ dateTimeToTimestamp
       hours = days * 24 + hh
       minutes = hours * 60 + mm
       seconds = minutes * 60 + ss
+
+-- 0 = Monday ... 6 = Sunday
+-- Function we came up with.
+dayOfWeek' :: Int -> Int -> Int -> Int
+dayOfWeek' y m d
+  | y > 1970 = (days+nullDate) `mod` 7
+  | otherwise = error "time did not exist before 1970"
+  where
+    yearDays = sum $ map (\x -> if isLeapYear x then 366 else 365) [1970 .. (y -1)]
+    monthDays = sum $ map (daysInMonth y) [1 .. (m -1)]
+    days = yearDays + monthDays + d
+    nullDate = 2
+
+-- Sakamoto's methods
+-- Taken from https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week
+dayOfWeek :: Int -> Int -> Int -> Int
+dayOfWeek y' m d = (y + y `div` 4 - y `div` 100 + y `div` 400 + (t !! (m - 1)) + d) `mod` 7
+  where
+    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+    y
+      | m < 4 = y' - 1
+      | otherwise = y'
+
+monthToStr :: Int -> String
+monthToStr 1 = "January"
+monthToStr 2 = "February"
+monthToStr 3 = "March"
+monthToStr 4 = "April"
+monthToStr 5 = "May"
+monthToStr 6 = "June"
+monthToStr 7 = "July"
+monthToStr 8 = "Augustus"
+monthToStr 9 = "September"
+monthToStr 10 = "October"
+monthToStr 11 = "November"
+monthToStr 12 = "December"
+monthToStr _ = "Invalid month"
+
+-- 0 = Monday ... 6 = Sunday
+dowToStr :: Int -> String
+dowToStr 0 = "Mon"
+dowToStr 1 = "Tue"
+dowToStr 2 = "Wed"
+dowToStr 3 = "Thu"
+dowToStr 4 = "Fri"
+dowToStr 5 = "Sat"
+dowToStr 6 = "Sun"
+dowToStr _ = "Invalid day of week"
--- assignment_1/src/Features.hs
+++ assignment_1/src/Features.hs
@@ -2,9 +2,12 @@ module Features where
 
 import Calendar
 import Data.List (isInfixOf)
+import Data.List.Split
 import Data.Maybe (fromJust)
 import DateTime
 import Text.PrettyPrint.Boxes
+import Text.Printf (printf)
+import Prelude hiding ((<>))
 
 -- Exercise 10
 countEvents :: Calendar -> Int
@@ -13,10 +16,10 @@ countEvents = length . events
 findEvents :: DateTime -> Calendar -> [Event]
 findEvents dt = filter f . events
   where
-    f e = dt >= dateTimeStart e && dt <= dateTimeEnd e
+    f e = dt >= dateTimeStart e && dt < dateTimeEnd e
 
 checkOverlapping :: Calendar -> Bool
-checkOverlapping = check . events -- volgensmij kun je hier een fold knallen maar idk
+checkOverlapping = check . events
   where
     check :: [Event] -> Bool
     check [] = False
@@ -38,20 +41,89 @@ timeSpent :: String -> Calendar -> Int
 timeSpent str = sum . map getTimeSpent . summaryFilter . events
   where
     summaryFilter :: [Event] -> [Event]
-    -- summaryFilter events = [e | e <- events, Just sum <- [summary e], str `isInfixOf` sum]
-    summaryFilter = filter (f . summary)
-      where
-        f x
-          | Just x <- x = str `isInfixOf` x
-          | otherwise = False
+    summaryFilter events = [e | e <- events, Just sum <- [summary e], str `isInfixOf` sum]
+    -- summaryFilter = filter (f . summary)
+    --   where
+    --     f x
+    --       | Just x <- x = str `isInfixOf` x
+    --       | otherwise = False
 
     getTimeSpent :: Event -> Int
     getTimeSpent e = diffMin (dateTimeStart e) (dateTimeEnd e)
       where
-        -- Pretty code
         diffMin :: DateTime -> DateTime -> Int
         diffMin dt1 dt2 = (dateTimeToTimestamp dt2 - dateTimeToTimestamp dt1) `div` 60
 
 -- Exercise 11
 ppMonth :: Year -> Month -> Calendar -> String
-ppMonth _ _ cal = printCalendar cal
+-- ppMonth y m cal = concatMap ((++) "\n" . show . summary) evs
+-- ppMonth y m cal = concatMap printDay [1 .. days]
+ppMonth y m cal = render $ vcat center1 [header, punctuateV left sepV $ map rowForDays weekChunks]
+  where
+    eventsToShow = 4
+    dayWidth = 24
+
+    -- TODO: Align calendar to always start on monday, it is a lot of work though.
+    weekChunks = chunksOf 7 [1 .. days]
+
+    header = alignVert center1 3 $ text (monthToStr (runMonth m) ++ " " ++ show (runYear y))
+
+    rowForDays :: [Int] -> Box
+    rowForDays days = punctuateH left sepH $ map boxDay days
+
+    boxDay :: Int -> Box
+    boxDay d = alignHoriz center1 dayWidth (alignHoriz left (dayWidth - 2) (headerBox // contentBox))
+      where
+        headerBox = text (show d ++ " " ++ dayStr)
+        dayStr = dowToStr (dayOfWeek' (runYear y) (runMonth m) d)
+        contentBox =
+          alignVert top eventsToShow $
+            vcat left (map boxEvent eventsToDisplay ++ footer)
+        events = eventsOnDay d
+        eventsToDisplay = take eventDisplayCount events
+        eventDisplayCount
+          | showDisplayMore = eventsToShow - 1
+          | otherwise = eventsToShow
+        showDisplayMore = length events > eventsToShow
+        footer
+          | showDisplayMore = [text $ "+ " ++ show (length events - eventDisplayCount) ++ " more"]
+          | otherwise = []
+
+    boxEvent :: Event -> Box
+    -- boxEvent e = text (eventName ++ " @ " ++ formatDt (dateTimeStart e))
+    boxEvent e = hsep 2 center1 [alignHoriz left nameFieldSize (text eventName), text (formatDt (dateTimeStart e))]
+      where
+        paddingSize = 2
+        timeFieldSize = 7
+        nameFieldSize = dayWidth - timeFieldSize - paddingSize
+        eventName
+          | Just x <- summary e =
+            if length x > nameFieldSize
+              then take (nameFieldSize - 1) x ++ "…"
+              else x
+          | otherwise = "Unnamed event"
+
+    formatDt :: DateTime -> String
+    formatDt dt = printf "%02d:%02d" h m
+      where
+        h = runHour . hour . time $ dt
+        m = runMinute . minute . time $ dt
+
+    sepH = vcat left $ replicate (eventsToShow + 1) $ char '|'
+    sepV = text $ concat $ sep : replicate 6 ('+' : sep)
+      where
+        sep = replicate dayWidth '-'
+
+    days = daysInMonth (runYear y) (runMonth m)
+    -- evs = filter f (events cal)
+    --   where
+    --     start = DateTime (Date y m (Day 0)) (Time (Hour 0) (Minute 0) (Second 0)) True
+    --     end = DateTime (Date y m (Day 32)) (Time (Hour 0) (Minute 0) (Second 0)) True
+    --     f e = dateTimeStart e > start && dateTimeStart e < end
+
+    eventsOnDay :: Int -> [Event]
+    eventsOnDay day = filter f (events cal)
+      where
+        start = DateTime (Date y m (Day day)) (Time (Hour 0) (Minute 0) (Second 0)) True
+        end = DateTime (Date y m (Day day)) (Time (Hour 24) (Minute 0) (Second 0)) True
+        f e = dateTimeStart e >= start && dateTimeStart e <= end
